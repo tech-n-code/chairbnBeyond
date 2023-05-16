4983385cd80640fdd3691ebfbc759b40796e1098
josh-benton <120067603+josh-benton@users.noreply.github.com>
2023-05-16 15:27:22 -0400

    added styling to component container class

--- client/src/components/Reviews/Reviews.jsx
+++ client/src/components/Reviews/Reviews.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import styles from "./Reviews.module.css";
 
 export default function Reviews() {
-    <div className={styles["component-container"]}>
-        
+  return (
+    <div className={styles["overall-section"]}>
+      <div className={styles["group-section"]}>
+        <div className={styles["component-container"]}>
+
+        </div>
+      </div>
     </div>
+  );
 }
