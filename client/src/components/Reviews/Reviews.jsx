20eb1d3504de4c4823fe7178dab8c986d9570d25
josh-benton <120067603+josh-benton@users.noreply.github.com>
2023-05-16 16:20:40 -0400

    styled reviews section header

--- client/src/components/Reviews/Reviews.jsx
+++ client/src/components/Reviews/Reviews.jsx
@@ -3,15 +3,24 @@ import styles from "./Reviews.module.css";
 
 export default function Reviews() {
   return (
-    <div className={styles["overall-section"]}>
+    <div className={styles["reviews-section"]}>
       <div className={styles["group-section"]}>
         <div className={styles["component-container"]}>
-            <div className={styles["component-subcontainer-1"]}>
-                <div className={styles["top-border"]}></div>
-                <div className={styles["component-subcontainer-2"]}>
-
+          <div className={styles["component-subcontainer-1"]}>
+            <div className={styles["top-border"]}></div>
+            <div className={styles["component-subcontainer-2"]}>
+              <section className={styles["lean-section"]}>
+                <div className={styles["title-container"]}>
+                    <span className={styles["star-container"]}>★</span>
+                    <span className={styles["rating-reviews"]}>
+                        <h2 className={styles["section-header"]}>
+                            <span className={styles["rating-reviews-text"]}>4.95 · 21 reviews</span>
+                        </h2>
+                    </span>
                 </div>
+              </section>
             </div>
+          </div>
         </div>
       </div>
     </div>
