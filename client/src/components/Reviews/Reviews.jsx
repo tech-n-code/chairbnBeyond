9b7b213c96c290e95d758efed3e95db9e81bf27f
josh-benton <120067603+josh-benton@users.noreply.github.com>
2023-05-16 15:09:27 -0400

    commented out review styling on index.css and styled component-container class

--- client/src/components/Reviews/Reviews.jsx
+++ client/src/components/Reviews/Reviews.jsx
@@ -2,5 +2,7 @@ import React from "react";
 import styles from "./Reviews.module.css";
 
 export default function Reviews() {
-    
+    <div className={styles["component-container"]}>
+        
+    </div>
 }
