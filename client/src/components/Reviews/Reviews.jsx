f692d3f45c5cb85d20d8014c20e2c7513806b678
josh-benton <120067603+josh-benton@users.noreply.github.com>
2023-05-16 17:16:30 -0400

    styled individual attribute and rating

--- client/src/components/Reviews/Reviews.jsx
+++ client/src/components/Reviews/Reviews.jsx
@@ -11,12 +11,35 @@ export default function Reviews() {
             <div className={styles["component-subcontainer-2"]}>
               <section className={styles["lean-section"]}>
                 <div className={styles["title-container"]}>
-                    <span className={styles["star-container"]}>★</span>
-                    <span className={styles["rating-reviews"]}>
-                        <h2 className={styles["section-header"]}>
-                            <span className={styles["rating-reviews-text"]}>4.95 · 21 reviews</span>
-                        </h2>
-                    </span>
+                  <span className={styles["star-container"]}>★</span>
+                  <span className={styles["rating-reviews"]}>
+                    <h2 className={styles["section-header"]}>
+                      <span className={styles["rating-reviews-text"]}>
+                        4.95 · 21 reviews
+                      </span>
+                    </h2>
+                  </span>
+                </div>
+                <div className={styles["attributes-section"]}>
+                  <div className={styles["attributes-container"]}>
+                    <div className={styles["attributes-lean-container"]}>
+                      <div className={styles["attribute-container-1"]}>
+                        <div className={styles["attribute-container-2"]}>
+                          <div className={styles["attribute-rating-container"]}>
+                            <div className={styles.attribute}>Cleanliness</div>
+                            <div className={styles["rating-container"]}>
+                              <div
+                                className={styles["rating-slider-container"]}
+                              >
+                                <span className={styles["rating-slider"]}></span>
+                              </div>
+                              <span className={styles["attribute-rating"]}>5.0</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </section>
             </div>
