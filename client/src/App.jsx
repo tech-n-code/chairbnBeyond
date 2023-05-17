d6d4e1ae4d5dc65d79c556f5017237a92a975d44
joegor85 <joe.gor85@gmail.com>
2023-05-17 13:14:47 -0500

    restructured and cleaned up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,15 @@
-import Amenities from "./components/Body/Amenities/Amenities";
+
 import "./index.css";
 import Header from "./components/Header/Header";
-import Map from "./components/Map/Map.jsx"
-import AboutThisPlace from "./components/AboutThisPlace/AboutThisPlace";
-import ChairCover from "./components/ChairCover/ChairCover.jsx";
-import ThingsToKnow from "./components/thingsToKnow/ThingsToKnow";
-import Gallery from "./components/gallery/Gallery"
-
+import Body from "./components/Body/Body";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Header />
-      <div className="titlebar">Property Title</div>
-      <div className="gallery"></div>
-      <Gallery/>
-      <div className="columnsContainer">
-        <div className="leftColumn">
-          <div className="overview">Property Overview Component</div>
-          <ChairCover />
-          <div className="host">Host Component</div>
-          <AboutThisPlace />
-          <Amenities />
-          <div className="calendar">Calendar Component</div>
-        </div>
-        <div className="rightColumn">
-          <div className="reservation">Reservation Component</div>
-        </div>
-      </div>
-      <div className="reviews">Reviews Component</div>
-
-      <div className="map">Map Component <Map/></div>
-
-      <ThingsToKnow className="things" />
-
-      <div className="footer">Footer</div>
+      <Body />
+      <Footer />
     </>
   );
 }
