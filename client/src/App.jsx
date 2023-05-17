c028a56d627a913a418e0af502ea7ccc74b52f43
MDrevon <MsDrevon@gmail.com>
2023-05-17 07:04:44 -0500

    Ready to test Amenities with Modal/restructure of Components folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import Amenities from "./components/amenities/amenities";
+import Amenities from "./components/Body/Amenities/Amenities";
 import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <header className="header">
---
+++ client/src/components/Body/Body.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Body.module.css";
+import Amenities from "./Amenities/Amenities";
+function Body() {
+  return <Amenities />;
+}
+
+export default Body;
