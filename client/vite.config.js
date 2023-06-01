ed629d6f58fc435ac5e2edac8d6b10eb9c7dc97d
Will Franceschini <113144533+tech-n-code@users.noreply.github.com>
2023-06-01 08:24:10 -0700

    added vite proxy server

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,4 +9,16 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './tests/setup.js',
   },
+  server: {
+    proxy: {
+      '/api/gallery/': 'http://localhost:3010',
+      '/api/title/': 'http://localhost:3050',
+      '/api/users/': 'http://localhost:4001',
+      '/api/hosts/': 'http://localhost:4001',
+      '/api/reviews/': 'http://localhost:3005',
+      'api/about/': 'http://localhost:3003',
+      '/api/amenities/': 'http://localhost:3002',
+      '/api/location/': 'http://localhost:4000',
+    },
+  }
 });
