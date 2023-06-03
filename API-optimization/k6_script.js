61125456b5492a3c7f115ea1ff4c0b83761c1e47
Will Franceschini <113144533+tech-n-code@users.noreply.github.com>
2023-06-03 12:22:28 -0700

    updated k6 test

--- API-optimization/k6_script.js
+++ API-optimization/k6_script.js
@@ -1,23 +1,57 @@
 import http from 'k6/http';
-
-import { check, sleep } from 'k6';
+import { sleep } from 'k6';
 
 export const options = {
-    vus: 3,
-    duration: '1m',
+    vus: 1,
+    // duration: '10s',
+    iterations: 1,
 };
 
 export default function () {
+  // const baseURL = 'https://192.168.1.167/api'; //will need the port for each route coded-in
+  // const baseURL = 'https://demo1.tech-n-code.com/api';
 
-  const urlRes = http.get('https://demo1.tech-n-code.com/1');
+  // const baseURL = 'http://192.168.1.111/api'; //will need the port for each route coded-in
+  // const baseURL = 'https://demo2.tech-n-code.com/api';
 
-  sleep(1);
+  // const baseURL = 'http://192.168.1.238/api'; //will need to change upstream server on nginx.conf 
+  const baseURL = 'https://demo3.tech-n-code.com/api';
+  
+  const listingId = 1;
 
+  const endpoints = [
+    `/title/${listingId}`, //3050
+    `/gallery/photo_url/${listingId}`, //3010
+    `/about/${listingId}`, //3003
+    `/amenities/${listingId}`, //3002
+    `/amenities/ten/${listingId}`, //3002
+    `/users/${listingId}`, //4001
+    `/hosts/about/${listingId}`, //4001
+    `/hosts/photo/${listingId}`, //4001
+    `/reviews/${listingId}`, //3005
+    `/location/${listingId}`, //4000
+    `/location/description/${listingId}`, //4000
+  ];
+
+  const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  };
+
+  for (const endpoint of endpoints) {
+    const url = `${baseURL}${endpoint}`;
+    const res = http.get(url, { headers });
+
+    // console.log('Status code:', res.status);
+    // console.log('Response body:', res.body);
+  
+    sleep(1);
+  }
 }
 
 /**
- * CLI command to run on docker k6:
+ * CLI command...
  * 
- * docker run --rm -i grafana/k6 run k6_script.js
+ * k6 run k6_script.js
  * 
  */
