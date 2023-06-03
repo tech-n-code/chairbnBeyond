c2b5df4e0a6f012bef7e73951bb26570220a94f3
Will Franceschini <113144533+tech-n-code@users.noreply.github.com>
2023-06-03 12:22:06 -0700

    added missing `/` to `api/about/` endpoint

--- client/vite.config.js
+++ client/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
       '/api/users/': 'http://localhost:4001',
       '/api/hosts/': 'http://localhost:4001',
       '/api/reviews/': 'http://localhost:3005',
-      'api/about/': 'http://localhost:3003',
+      '/api/about/': 'http://localhost:3003',
       '/api/amenities/': 'http://localhost:3002',
       '/api/location/': 'http://localhost:4000',
     },
